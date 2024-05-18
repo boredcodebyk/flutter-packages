c071d7482c5667d5db8ebc1da999db66bb554499
boredcodebyk <25322315+boredcodebyk@users.noreply.github.com>
2024-05-18 11:24:28 +0530

    attempt to bring new colors to dynamic_color

--- packages/dynamic_color/lib/src/corepalette_to_colorscheme.dart
+++ packages/dynamic_color/lib/src/corepalette_to_colorscheme.dart
@@ -6,46 +6,97 @@ extension CorePaletteToColorScheme on CorePalette {
   ColorScheme toColorScheme({
     Brightness brightness = Brightness.light,
   }) {
-    final Scheme scheme;
+    final SchemeTonalSpot scheme;
 
     switch (brightness) {
       case Brightness.light:
-        scheme = Scheme.lightFromCorePalette(this);
+        scheme = SchemeTonalSpot(
+            sourceColorHct: Hct.fromInt(primary.keyColor.toInt()),
+            isDark: false,
+            contrastLevel: 0.0);
         break;
       case Brightness.dark:
-        scheme = Scheme.darkFromCorePalette(this);
+        scheme = SchemeTonalSpot(
+            sourceColorHct: Hct.fromInt(primary.keyColor.toInt()),
+            isDark: true,
+            contrastLevel: 0.0);
         break;
     }
     return ColorScheme(
-      primary: Color(scheme.primary),
-      onPrimary: Color(scheme.onPrimary),
-      primaryContainer: Color(scheme.primaryContainer),
-      onPrimaryContainer: Color(scheme.onPrimaryContainer),
-      secondary: Color(scheme.secondary),
-      onSecondary: Color(scheme.onSecondary),
-      secondaryContainer: Color(scheme.secondaryContainer),
-      onSecondaryContainer: Color(scheme.onSecondaryContainer),
-      tertiary: Color(scheme.tertiary),
-      onTertiary: Color(scheme.onTertiary),
-      tertiaryContainer: Color(scheme.tertiaryContainer),
-      onTertiaryContainer: Color(scheme.onTertiaryContainer),
-      error: Color(scheme.error),
-      onError: Color(scheme.onError),
-      errorContainer: Color(scheme.errorContainer),
-      onErrorContainer: Color(scheme.onErrorContainer),
-      outline: Color(scheme.outline),
-      outlineVariant: Color(scheme.outlineVariant),
-      background: Color(scheme.background),
-      onBackground: Color(scheme.onBackground),
-      surface: Color(scheme.surface),
-      onSurface: Color(scheme.onSurface),
-      surfaceVariant: Color(scheme.surfaceVariant),
-      onSurfaceVariant: Color(scheme.onSurfaceVariant),
-      inverseSurface: Color(scheme.inverseSurface),
-      onInverseSurface: Color(scheme.inverseOnSurface),
-      inversePrimary: Color(scheme.inversePrimary),
-      shadow: Color(scheme.shadow),
-      scrim: Color(scheme.scrim),
+      primary: Color(MaterialDynamicColors.primary.getArgb(scheme)),
+      onPrimary: Color(MaterialDynamicColors.onPrimary.getArgb(scheme)),
+      primaryContainer:
+          Color(MaterialDynamicColors.primaryContainer.getArgb(scheme)),
+      onPrimaryContainer:
+          Color(MaterialDynamicColors.onPrimaryContainer.getArgb(scheme)),
+      primaryFixed: Color(MaterialDynamicColors.primaryFixed.getArgb(scheme)),
+      primaryFixedDim:
+          Color(MaterialDynamicColors.primaryFixedDim.getArgb(scheme)),
+      onPrimaryFixed:
+          Color(MaterialDynamicColors.onPrimaryFixed.getArgb(scheme)),
+      onPrimaryFixedVariant:
+          Color(MaterialDynamicColors.onPrimaryFixedVariant.getArgb(scheme)),
+      secondary: Color(MaterialDynamicColors.secondary.getArgb(scheme)),
+      onSecondary: Color(MaterialDynamicColors.onSecondary.getArgb(scheme)),
+      secondaryContainer:
+          Color(MaterialDynamicColors.secondaryContainer.getArgb(scheme)),
+      onSecondaryContainer:
+          Color(MaterialDynamicColors.onSecondaryContainer.getArgb(scheme)),
+      secondaryFixed:
+          Color(MaterialDynamicColors.secondaryFixed.getArgb(scheme)),
+      secondaryFixedDim:
+          Color(MaterialDynamicColors.secondaryFixedDim.getArgb(scheme)),
+      onSecondaryFixed:
+          Color(MaterialDynamicColors.onSecondaryFixed.getArgb(scheme)),
+      onSecondaryFixedVariant:
+          Color(MaterialDynamicColors.onSecondaryFixedVariant.getArgb(scheme)),
+      tertiary: Color(MaterialDynamicColors.tertiary.getArgb(scheme)),
+      onTertiary: Color(MaterialDynamicColors.onTertiary.getArgb(scheme)),
+      tertiaryContainer:
+          Color(MaterialDynamicColors.tertiaryContainer.getArgb(scheme)),
+      onTertiaryContainer:
+          Color(MaterialDynamicColors.onTertiaryContainer.getArgb(scheme)),
+      tertiaryFixed: Color(MaterialDynamicColors.tertiaryFixed.getArgb(scheme)),
+      tertiaryFixedDim:
+          Color(MaterialDynamicColors.tertiaryFixedDim.getArgb(scheme)),
+      onTertiaryFixed:
+          Color(MaterialDynamicColors.onTertiaryFixed.getArgb(scheme)),
+      onTertiaryFixedVariant:
+          Color(MaterialDynamicColors.onTertiaryFixedVariant.getArgb(scheme)),
+      error: Color(MaterialDynamicColors.error.getArgb(scheme)),
+      onError: Color(MaterialDynamicColors.onError.getArgb(scheme)),
+      errorContainer:
+          Color(MaterialDynamicColors.errorContainer.getArgb(scheme)),
+      onErrorContainer:
+          Color(MaterialDynamicColors.onErrorContainer.getArgb(scheme)),
+      outline: Color(MaterialDynamicColors.outline.getArgb(scheme)),
+      outlineVariant:
+          Color(MaterialDynamicColors.outlineVariant.getArgb(scheme)),
+      surface: Color(MaterialDynamicColors.surface.getArgb(scheme)),
+      surfaceDim: Color(MaterialDynamicColors.surfaceDim.getArgb(scheme)),
+      surfaceBright: Color(MaterialDynamicColors.surfaceBright.getArgb(scheme)),
+      surfaceContainerLowest:
+          Color(MaterialDynamicColors.surfaceContainerLowest.getArgb(scheme)),
+      surfaceContainerLow:
+          Color(MaterialDynamicColors.surfaceContainerLow.getArgb(scheme)),
+      surfaceContainer:
+          Color(MaterialDynamicColors.surfaceContainer.getArgb(scheme)),
+      surfaceContainerHigh:
+          Color(MaterialDynamicColors.surfaceContainerHigh.getArgb(scheme)),
+      surfaceContainerHighest:
+          Color(MaterialDynamicColors.surfaceContainerHighest.getArgb(scheme)),
+      onSurface: Color(MaterialDynamicColors.onSurface.getArgb(scheme)),
+      onSurfaceVariant:
+          Color(MaterialDynamicColors.onSurfaceVariant.getArgb(scheme)),
+      inverseSurface:
+          Color(MaterialDynamicColors.inverseSurface.getArgb(scheme)),
+      onInverseSurface:
+          Color(MaterialDynamicColors.inverseOnSurface.getArgb(scheme)),
+      inversePrimary:
+          Color(MaterialDynamicColors.inversePrimary.getArgb(scheme)),
+      shadow: Color(MaterialDynamicColors.shadow.getArgb(scheme)),
+      scrim: Color(MaterialDynamicColors.scrim.getArgb(scheme)),
+      surfaceTint: Color(MaterialDynamicColors.primary.getArgb(scheme)),
       brightness: brightness,
     );
   }
